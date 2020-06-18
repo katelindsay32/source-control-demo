e953cfaa533f287c060d3230fb02fa62dbaefffe
Sickora, Kate  - C <sickora.kate@principal.com>
2020-06-18 09:48:36 -0500

    feature 2

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -3,5 +3,10 @@ package com;
 public class Application {
 	public static void main(String[] args) {
 		System.out.println("Source control demo, hello!");
+		feature2();
+	}
+
+	private static void feature2() {
+		System.out.println("feature2 is atually better!");
 	}
 }
